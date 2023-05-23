6df34c5dfa17479fb2ef0ecc9be2e5870376ce38
Vit <skala_77@inbox.ru>
2023-05-23 21:54:30 +0300

    Первое задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+    let result = users.filter(user => user.gender === gender).map(elem => elem.age).reduce((acc, item, index, arr) => {
+       acc += item;
+        if(index === arr.length-1){
+    return acc / arr.length;
+   }
+        return acc;
+   }, 0)
+   return result;
+   }
