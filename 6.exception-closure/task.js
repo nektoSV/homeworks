47d713a3a2dca107eacb14ade16c8047df807886
Vit <skala_77@inbox.ru>
2023-06-06 17:03:05 +0300

    six task completed

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,55 @@
-﻿
+﻿function parseCount(number){
+    const parsedValue = Number.parseFloat(number);
+    if(isNaN(parsedValue)) {
+        throw new Error('Невалидное значение');
+    }
+    return parsedValue;
+}
+
+function validateCount(number){
+    try {
+         return parseCount(number);
+        
+    } catch (error) {
+        return error;
+    }
+}
+
+
+class Triangle {
+    constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+    if(a + b < c || b  + c < a || a + c < b) {
+        throw new Error('Треугольник с такими сторонами не существует');
+    }
+    
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        let p = this.perimeter / 2;
+        return Number((Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3));
+    }
+}
+
+    function getTriangle(a, b, c) {
+        try {
+            return new Triangle(a, b, c)
+        }catch (error) {
+            return  {
+                get perimeter() {
+                    return 'Ошибка! Треугольник не существует';
+                },
+
+                get area() {
+                    return 'Ошибка! Треугольник не существует';
+                }
+            }
+        }
+    }
