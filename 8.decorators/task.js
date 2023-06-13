18c7a17c60cf56eb7cfa2e11084c08628509e63f
Vit <skala_77@inbox.ru>
2023-06-13 19:58:52 +0300

    eighth task completed

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,52 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = md5(args);
+        let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) { 
+        console.log("Из кэша: " + objectInCache.result); 
+        return "Из кэша: " + objectInCache.result;
+    }
+
+    let result = func(...args); 
+    cache.push({hash, result}); 
+    if (cache.length > 5) { 
+      cache.shift(); 
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
 }
+return wrapper;
+}
+    
 
 //Задача № 2
-function debounceDecoratorNew(func, delay) {
+
+  function debounceDecoratorNew(func, delay) {
+    let timeoutId = null;
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+
   
-}
+    function wrapper(...args) {
+        wrapper.allCount += 1;
+        if(timeoutId) {
+            clearTimeout(timeoutId);
+      }
+
+        if(!timeoutId) {
+            func(...args)
+            wrapper.count += 1;
+        }
+
+        timeoutId = setTimeout(() => {
+            func(...args);
+            timeoutId = null;
+        },delay);
+  }
+  return wrapper;
+  }
+  
